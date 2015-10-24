bfbeb5bb4b6313034179ea96c09b12f9f48340d2
Steve Journeay <sjournea@taralaur.com>
2015-10-23 21:48:12 -0700

    comment out delay to finish later

--- BBGame/BBGame.swift
+++ BBGame/BBGame.swift
@@ -140,14 +140,13 @@ class BBGame : Game {
       makeSelection = true
       selectionDisplay!.enableSelection(true)
     } else {
-      let time = dispatch_time(dispatch_time_t(DISPATCH_TIME_NOW), 2 * Int64(NSEC_PER_SEC))
-      print("Delay of 2 seconds before selection")
-      dispatch_after(time, dispatch_get_main_queue()) {
+//      let time = dispatch_time(dispatch_time_t(DISPATCH_TIME_NOW), 2 * Int64(NSEC_PER_SEC))
+//      dispatch_after(time, dispatch_get_main_queue())
+//      {
         // put your code which should be executed with a delay here
         let idx = team.pickSelection(self)
         self.in_play(idx)
-        print("Delay complete")
-      }
+//      }
     }
     
     return EVENT
